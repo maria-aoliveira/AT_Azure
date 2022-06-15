07b35cfe29a6a512d829c4917521f06d673fba1d
Maria Eduarda <maria.aoliveira@al.infnet.edu.br>
2022-06-15 01:09:36 -0300

    first blood

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace AT_Maria_DR4
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
